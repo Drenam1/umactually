71730003323ce4befc358aa01be59af998d1e023
Drenam1 <bobthetomato29@gmail.com>
2025-06-15 21:23:00 +0100

    Fixed lint errors

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD5eAhYwWvZWT2HedmRom-ZUFd_TVC-S_U",
