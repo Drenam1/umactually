f4a59869c63c2dc27e1bf1e0d15de0c09c69912d
Drenam1 <bobthetomato29@gmail.com>
2025-06-16 15:14:24 +0100

    Return to monke

--- src/index.tsx
+++ src/index.tsx
@@ -5,31 +5,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { firebaseConfig as secretConfig } from "./secrets";
-
-const {
-  REACT_APP_FIREBASE_API_KEY,
-  REACT_APP_FIREBASE_AUTH_DOMAIN,
-  REACT_APP_FIREBASE_DATABASE_URL,
-  REACT_APP_FIREBASE_PROJECT_ID,
-  REACT_APP_FIREBASE_STORAGE_BUCKET,
-  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  REACT_APP_FIREBASE_APP_ID,
-  REACT_APP_FIREBASE_MEASUREMEMT_ID,
-} = process.env;
 
 const firebaseConfig = {
-  apiKey: REACT_APP_FIREBASE_API_KEY ?? secretConfig.apiKey,
-  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN ?? secretConfig.authDomain,
-  databaseURL: REACT_APP_FIREBASE_DATABASE_URL ?? secretConfig.databaseURL,
-  projectId: REACT_APP_FIREBASE_PROJECT_ID ?? secretConfig.projectId,
-  storageBucket:
-    REACT_APP_FIREBASE_STORAGE_BUCKET ?? secretConfig.storageBucket,
-  messagingSenderId:
-    REACT_APP_FIREBASE_MESSAGING_SENDER_ID ?? secretConfig.messagingSenderId,
-  appId: REACT_APP_FIREBASE_APP_ID ?? secretConfig.appId,
-  measurementId:
-    REACT_APP_FIREBASE_MEASUREMEMT_ID ?? secretConfig.measurementId,
+  apiKey: "AIzaSyD5eAhYwWvZWT2HedmRom-ZUFd_TVC-S_U",
+  authDomain: "umactually-925d4.firebaseapp.com",
+  databaseURL: "https://umactually-925d4-default-rtdb.firebaseio.com",
+  projectId: "umactually-925d4",
+  storageBucket: "umactually-925d4.firebasestorage.app",
+  messagingSenderId: "44152076569",
+  appId: "1:44152076569:web:d994024c8ca5ded76dad88",
+  measurementId: "G-K77MMQHNJG",
 };
 
 // Initialize Firebase
