425c4fd5aea7fbc162439d53e18e5a01b1e9ccf6
Drenam1 <bobthetomato29@gmail.com>
2025-06-16 14:43:29 +0100

    Builddeploy

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import { initializeApp } from "firebase/app";
 const {
   REACT_APP_FIREBASE_API_KEY,
   REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
   REACT_APP_FIREBASE_PROJECT_ID,
   REACT_APP_FIREBASE_STORAGE_BUCKET,
   REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
@@ -21,7 +22,7 @@ console.log(process.env);
 const firebaseConfig = {
   apiKey: REACT_APP_FIREBASE_API_KEY,
   authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: "https://umactually-925d4-default-rtdb.firebaseio.com",
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
   projectId: REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
