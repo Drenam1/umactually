16483091c73999b220b6428d428e92ad69baab91
Drenam1 <bobthetomato29@gmail.com>
2025-06-16 14:50:25 +0100

    Builddeploy

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { firebaseConfig as secretConfig } from "./secrets";
 
 const {
   REACT_APP_FIREBASE_API_KEY,
@@ -17,17 +18,18 @@ const {
   REACT_APP_FIREBASE_MEASUREMEMT_ID,
 } = process.env;
 
-console.log(process.env);
-
 const firebaseConfig = {
-  apiKey: REACT_APP_FIREBASE_API_KEY,
-  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: REACT_APP_FIREBASE_APP_ID,
-  measurementId: REACT_APP_FIREBASE_MEASUREMEMT_ID,
+  apiKey: REACT_APP_FIREBASE_API_KEY ?? secretConfig.apiKey,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN ?? secretConfig.authDomain,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL ?? secretConfig.databaseURL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID ?? secretConfig.projectId,
+  storageBucket:
+    REACT_APP_FIREBASE_STORAGE_BUCKET ?? secretConfig.storageBucket,
+  messagingSenderId:
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID ?? secretConfig.messagingSenderId,
+  appId: REACT_APP_FIREBASE_APP_ID ?? secretConfig.appId,
+  measurementId:
+    REACT_APP_FIREBASE_MEASUREMEMT_ID ?? secretConfig.measurementId,
 };
 
 // Initialize Firebase
