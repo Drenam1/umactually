67f0031a5010f3fb74bac249692062c4876f9c0e
Drenam1 <bobthetomato29@gmail.com>
2025-06-16 14:05:29 +0100

    Refactor Firebase configuration: move firebaseConfig to a separate secrets file for improved security

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD5eAhYwWvZWT2HedmRom-ZUFd_TVC-S_U",
-  authDomain: "umactually-925d4.firebaseapp.com",
-  databaseURL: "https://umactually-925d4-default-rtdb.firebaseio.com",
-  projectId: "umactually-925d4",
-  storageBucket: "umactually-925d4.firebasestorage.app",
-  messagingSenderId: "44152076569",
-  appId: "1:44152076569:web:d994024c8ca5ded76dad88",
-  measurementId: "G-K77MMQHNJG",
-};
+import { firebaseConfig } from "../.config/secrets";
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
