b6171804596d2d5b91ac5119e7d9ba091ceaefa7
Drenam1 <bobthetomato29@gmail.com>
2025-06-16 14:13:08 +0100

    Environment

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "./.config/secrets";
+
+const firebaseConfig = {
+  apiKey: process.env.GOOGLE_API_KEY,
+  authDomain: "umactually-925d4.firebaseapp.com",
+  databaseURL: "https://umactually-925d4-default-rtdb.firebaseio.com",
+  projectId: "umactually-925d4",
+  storageBucket: "umactually-925d4.firebasestorage.app",
+  messagingSenderId: "44152076569",
+  appId: "1:44152076569:web:d994024c8ca5ded76dad88",
+  measurementId: "G-K77MMQHNJG",
+};
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
